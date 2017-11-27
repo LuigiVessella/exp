88ba85f6da3ae00f4c63ff8cbbf79bd51d70254a
LuigiVessella <vessellaluigi12@gmail.com>
2017-11-27 18:08:17 +0100

    aggiustamenti

--- nepero2.c
+++ nepero2.c
@@ -4,13 +4,13 @@ int main(){
 	int fat=1, i;
 	double ris=1.0, dif;
 
-	for(i=1; i<10; i++){
-		dif=dif;
+	for(i=1; i<=10; i++){
+		dif=ris;
 		fat=fat*i;
 	 	ris=ris+(1./(double)fat);
                 printf("e: %f\n",ris);	
-
-	       	if(ris-(int)ris >= 0.718)
+		
+		if((dif-ris)*-1<0.001)
 			break;
 	}
 
